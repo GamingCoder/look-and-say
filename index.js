7e4552eaf01a513d24d4d153db82251ca1a5c840
GamingCoder <coreoder@gmail.com>
2016-11-06 11:59:48 +0100

    Fix everything

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 module.exports = function(seed, iterations) {
-	if (iterations === undefined){iterations = 1;}
-	r = [seed];
-	
+	iterations = iterations || 1;
+	r = [seed.toString()];
+
+	function reduceNumber(prev,cur){
+		if(!prev[0]) prev[0] = [cur,1];
+		else if (prev[prev.length-1][0] === cur) prev[prev.length-1][1]++;
+		else prev[prev.length] = [cur,1];
+		return prev;
+	}
+
+	for(var i=1; i<=iterations; i++){
+		r[i] = r[i-1]
+			.split('')
+			.reduce(reduceNumber, [])
+			.map(function(e){
+				return e[1]+e[0];
+			})
+			.join('');
+	}
+
 	return r;
-}
+};
